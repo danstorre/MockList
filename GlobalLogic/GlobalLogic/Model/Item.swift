4681511c5382d6b0d88f85a2e2a81e9aaf6c27e1
Daniel Torres <danstorre@gmail.com>
2020-05-30 15:10:24 -0300

    add decoding functionality to Item model.

--- GlobalLogic/GlobalLogic/Model/Item.swift
+++ GlobalLogic/GlobalLogic/Model/Item.swift
@@ -6,10 +6,47 @@
 //  Copyright © 2020 dansTeam. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-struct Item{
+enum ItemError: Error{
+    case ErrorImageUrlIsNotValid
+}
+
+struct Item: Decodable{
     var title: String
     var description: String
     var thumbnail: URL?
+    enum CodingKeys: String, CodingKey {
+        case title
+        case description
+        case thumbnail = "image"
+    }
+    
+    init(title: String, description: String) {
+        self.title = title
+        self.description = description
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        title = try values.decode(String.self, forKey: .title)
+        description = try values.decode(String.self, forKey: .description)
+        
+        guard let urlString =
+            try values.decodeIfPresent(String.self,
+                                       forKey: .thumbnail)
+            else { return }
+        
+        
+        if let url = URL(string: urlString),
+            UIApplication.shared.canOpenURL(url) {
+            thumbnail = url
+        }else {
+            throw ItemError.ErrorImageUrlIsNotValid
+        }
+        
+    }
+    
 }
+
+
--- GlobalLogic/GlobalLogicTests/ItemTests.swift
+++ GlobalLogic/GlobalLogicTests/ItemTests.swift
@@ -31,4 +31,43 @@ class ItemTests: XCTestCase {
         XCTAssertNotNil(sut.thumbnail)
     }
     
+    func testInit_whenDecodingItemWithJsonDecoder_ShouldSetTitleAndSetDescription(){
+        let data = ["title": "Item 1",
+        "description": "Lorem "]
+        let item = try! returnItemDataFromDict(data)
+        
+        XCTAssertEqual(item.title, "Item 1")
+        XCTAssertEqual(item.description, "Lorem ")
+        XCTAssertNil(item.thumbnail)
+    }
+    
+    func testInit_whenPassingAJsonWithThumbnail_ShouldSetAThumnail(){
+        let data = ["title": "Item 1",
+        "description": "Lorem ",
+        "image": "https://picsum.photos/100/100?image=0"]
+        let item = try! returnItemDataFromDict(data)
+        
+        XCTAssertEqual(item.title, "Item 1")
+        XCTAssertEqual(item.description, "Lorem ")
+        XCTAssertEqual(item.thumbnail, URL(string: "https://picsum.photos/100/100?image=0"))
+    }
+    
+    func testInit_WhenImageUrlDataIsCorrupted_ThrowsErrorUrlIsNotValid(){
+        let data = ["title": "Item 1",
+        "description": "Lorem ",
+        "image": "httpspicsum.photos/100/100mage=0"]
+        
+        XCTAssertThrowsError(try returnItemDataFromDict(data), "", { (error) in
+            XCTAssertTrue(error is ItemError)
+            XCTAssertEqual(error as! ItemError, ItemError.ErrorImageUrlIsNotValid)
+        })
+    }
+    
+    func returnItemDataFromDict(_ dict: [String: String]) throws -> Item {
+        let jsonEncoder = JSONEncoder()
+        let dataJson = try! jsonEncoder.encode(dict)
+        let JsonDecoder = JSONDecoder()
+        return try JsonDecoder.decode(Item.self, from: dataJson)
+    }
+    
 }
