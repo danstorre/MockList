a20a5dc6c714b45e57059a37d3eab7854d251096
Daniel Torres <danstorre@gmail.com>
2020-06-01 19:35:54 -0300

    refactor.

--- GlobalLogic/GlobalLogic/Controller/ItemFetcher.swift
+++ GlobalLogic/GlobalLogic/Controller/ItemFetcher.swift
@@ -8,32 +8,6 @@
 
 import UIKit
 
-enum DownSamplerMethods {
-    static func downsample(imageAt imageURL: URL,
-                              to pointSize: CGSize? = CGSize(width: 100, height: 100),
-                              scale: CGFloat? = 3) -> UIImage? {
-           let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
-           guard let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, imageSourceOptions) else{
-               return nil
-           }
-           var downsampleOptions: CFDictionary
-           if let pointSize = pointSize,
-               let scale = scale {
-               let maxDimensionInPixels = max(pointSize.width, pointSize.height) * scale
-               downsampleOptions = [kCGImageSourceCreateThumbnailFromImageAlways: true,
-                                    kCGImageSourceShouldCacheImmediately: true,
-                                    kCGImageSourceCreateThumbnailWithTransform: true,
-                                    kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels] as CFDictionary
-           }else {
-               downsampleOptions = [kCGImageSourceCreateThumbnailFromImageAlways: kCFBooleanTrue,
-                                    kCGImageSourceShouldCacheImmediately: kCFBooleanTrue,
-                                    kCGImageSourceCreateThumbnailWithTransform: kCFBooleanTrue] as CFDictionary
-           }
-           let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions)!
-           return UIImage(cgImage: downsampledImage)
-       }
-}
-
 enum APIProtocolError: Error {
     case FetchedImageNotFound
 }
@@ -59,8 +33,6 @@ extension APIProtocol {
             }
         }
     }
-    
-   
 }
 
 extension ListTableViewController: ItemFetcherDelegate{
